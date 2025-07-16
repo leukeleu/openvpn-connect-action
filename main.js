0c9a405944766f855525c24c131b56844195bdfa
Wouter de Vries <wdevries@leukeleu.nl>
2025-07-16 11:10:54 +0200

    Install OpenVPN

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 import * as core from '@actions/core';
 import { writeFile } from 'fs/promises';
+import { execSync } from 'child_process';
+
+function installOpenVPN() {
+  try {
+    core.info('Installing OpenVPN...');
+    execSync('sudo apt-get update && sudo apt-get install -y openvpn', {stdio: 'inherit'});
+    core.info('OpenVPN installed.');
+  } catch (error) {
+    core.setFailed(`Failed to install package: ${error.message}`);
+  }
+}
 
 async function run() {
   try {
     core.info('Running main action logic...');
+    installOpenVPN();
 
     const tempFile = '/tmp/demo-esm.txt';
     await writeFile(tempFile, 'Temporary data (ESM)');
